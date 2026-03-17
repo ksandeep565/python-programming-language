0116820106162469e0f3405be1999a36626fbc78
ksandeep565 <ksandeepsandeep126@gmail.com>
2026-03-17 15:26:33 +0530

    Add files via upload

---
+++ if statement.py
@@ -0,0 +1,10 @@
+lst=int(input("enter the value:"))
+if i==60:
+    print("good boy")
+if i>60:
+    print("bad boy")
+if i<39:
+    print("chandu is not bad")
+output:
+enter the value:37
+good boy
