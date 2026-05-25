8a8f45f5dc059441fef7c15033da095ec8bb50f7
ksandeep565 <ksandeepsandeep126@gmail.com>
2026-05-25 09:42:57 +0530

    Add files via upload

---
+++ student_marks.py
@@ -0,0 +1,21 @@
+# Python program to print marks of 3 students in college
+
+# Student 1
+name1 = "Ravi"
+marks1 = 85
+
+# Student 2
+name2 = "Sita"
+marks2 = 90
+
+# Student 3
+name3 = "Rahul"
+marks3 = 78
+
+# Printing student details
+print("Student Marks Details")
+print("----------------------")
+
+print("Name:", name1, " Marks:", marks1)
+print("Name:", name2, " Marks:", marks2)
+print("Name:", name3, " Marks:", marks3)
